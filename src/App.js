9b56ea97d44a2b9cec9e7ac558c3023f928858d0
Marton Reka <rmarton910@gmail.com>
2020-02-07 11:35:18 +0200

    app refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Program from "./program";
+import Formular from "./formular";
 
 import "./App.css";
 
@@ -32,10 +33,31 @@ class App extends Component {
       ]
     };
 
+    this.adaugActivitate = this.adaugActivitate.bind(this);
     this.stergeActivitate = this.stergeActivitate.bind(this);
   }
 
-  stergeActivitate(ev) {}
+  adaugActivitate(act) {
+    const { activitati } = this.state;
+    var sirAct = activitati;
+    sirAct.push({
+      id: sirAct.length ? sirAct[sirAct.length - 1].id + 1 : 1,
+      ora: act.ora,
+      titlu: act.titlu,
+      loc: act.loc,
+      descriere: act.descriere
+    });
+    this.setState({ activitati: sirAct });
+  }
+
+  stergeActivitate(ev) {
+    const idSup = parseInt(ev.target.id); //  id e convertit in intreg
+    const { activitati } = this.state;
+    const sirAct = activitati.filter(item => {
+      return item.id !== idSup;
+    });
+    this.setState({ activitati: sirAct });
+  }
 
   render() {
     return (
@@ -44,6 +66,7 @@ class App extends Component {
           activitati={this.state.activitati}
           stergeActivitate={this.stergeActivitate}
         />
+        <Formular adaugActivitate={this.adaugActivitate} />
       </div>
     );
   }
